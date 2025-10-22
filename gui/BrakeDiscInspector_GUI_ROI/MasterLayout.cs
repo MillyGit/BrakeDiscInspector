2f421e1deded38bc465c86234c6951514eb27314
MillyGit <correo.7304@gmail.com>
2025-10-22 13:47:00 -0700

    Fix: Add missing namespace for DatasetPreviewItem to resolve CS0246 and CS1503 errors

--- gui/BrakeDiscInspector_GUI_ROI/MasterLayout.cs
+++ gui/BrakeDiscInspector_GUI_ROI/MasterLayout.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using BrakeDiscInspector_GUI_ROI.Workflow;
 
 namespace BrakeDiscInspector_GUI_ROI
 {
